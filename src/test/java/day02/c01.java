4ea147c6fa805e35abd963253c27ee58c653dd0b
“Huseyin <sen19huseyin@gmail.com>
2023-02-28 00:04:11 +0300

    c01

--- src/test/java/day02/c01.java
+++ src/test/java/day02/c01.java
@@ -1,12 +1,24 @@
 package day02;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.time.Duration;
+
 public class c01 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
     System.setProperty("Webdriver.chrome.driver","src/drivers/chromedriver.exe");
         WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
         driver.get("https://www.amazon.com");
+        WebElement aramaKutusu=driver.findElement(By.id("twotabsearchtextbox"));
+        aramaKutusu.sendKeys("toshiba laptop");
+        aramaKutusu.submit();
+
+
+
     }
 }
